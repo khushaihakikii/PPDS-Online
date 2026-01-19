38db81de7cab32f24e4f02ec793ef5ab9a19708d
Khusai Hakikyy <khusai.hakiky1932@gmail.com>
2026-01-19 14:53:04 +0700

    gtw

--- routes/web.php
+++ routes/web.php
@@ -20,12 +20,13 @@ Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
 
+Route::view('/panduan', 'pages.panduan')->name('pages.panduan');
+
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 
-    Route::view('/panduan', 'pages.panduan')->name('pages.panduan');
 
     // Routes untuk pendaftaran
     Route::resource('registrations', \App\Http\Controllers\StudentRegistrationController::class);
